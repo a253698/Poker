8f2b7b1e947ecafc69b8a944fac1efc910df4dfc
Mavis <angel253698@gmail.com>
2018-12-07 15:33:53 +0800

    extends

--- app/src/main/java/mavis/com/poker/MainActivity.java
+++ app/src/main/java/mavis/com/poker/MainActivity.java
@@ -1,10 +1,13 @@
 package mavis.com.poker;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 //ctrl + alt + L 自動排版
 
@@ -19,11 +22,28 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
-    class PokerAdapter{
-        class PokerHolder extends RecyclerView.ViewHolder{
+    class PokerAdapter extends RecyclerView.Adapter<PokerAdapter.PokerHolder>{
+        @NonNull
+        @Override
+        public PokerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return null;
+        }
 
+        @Override
+        public void onBindViewHolder(@NonNull PokerHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+        class PokerHolder extends RecyclerView.ViewHolder{
+            TextView pokerView;
             public PokerHolder(View itemView) {
                 super(itemView);
+                pokerView = itemView.findViewById(R.id.tv_poker);
             }
         }
     }
