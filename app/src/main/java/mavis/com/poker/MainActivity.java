476854c7f68608e13c742587ae6700621e419123
Mavis <angel253698@gmail.com>
2018-12-07 15:26:59 +0800

    ViewHolder Adapter

--- app/src/main/java/mavis/com/poker/MainActivity.java
+++ app/src/main/java/mavis/com/poker/MainActivity.java
@@ -2,6 +2,9 @@ package mavis.com.poker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 //ctrl + alt + L 自動排版
 
@@ -11,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    class PokerAdapter{
+        class PokerHolder extends RecyclerView.ViewHolder{
+
+            public PokerHolder(View itemView) {
+                super(itemView);
+            }
+        }
     }
 }
